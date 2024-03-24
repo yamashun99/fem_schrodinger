f23e5dc6f59ec29bc10c658897c8b8944a74fe38
yamashun99 <yamashun.103099@gmail.com>
2024-03-24 13:33:44 +0900

    有限要素内のポテンシャルの入れ方を変更

--- src/fem_1d_eigen.bk.py
+++ src/fem_1d_eigen.bk.py
@@ -4,15 +4,16 @@ import numpy as np
 class Fem1dEigen:
     """１次元固有値問題 (d^2/d^2x + V)u = λu を解く"""
 
-    def __init__(self, xs):
+    def __init__(self, xs, v):
         # Parameters for K matrix
-        self.size = len(xs)
+        self.size = len(v)  # Using the same size as the previous matrix for consistency
         self.xs = xs
         self.xs_next = np.roll(xs, -1)  # 次の座標点
         self.hs = self.xs_next - xs  # 要素の長さ
         self.hs = self.hs[:-1]
+        self.v = v
 
-    def get_K0(self):
+    def get_K(self):
         # Initialize the K matrix with zeros
         K = np.zeros((self.size, self.size))
 
@@ -47,34 +48,19 @@ class Fem1dEigen:
 
         return M / 6
 
-    # def get_K1(self, v):
-    #    K = np.zeros((self.size, self.size))
-    #    for i in range(self.size):
-    #        if i == 0:
-    #            K[i, i] = (3 * v[i] + v[i + 1]) * self.hs[i]
-    #            K[i, i + 1] = (v[i] + v[i + 1]) * self.hs[i]
-    #        elif i == self.size - 1:
-    #            K[i, i - 1] = (v[i - 1] + v[i]) * self.hs[i - 1]
-    #            K[i, i] = (v[i - 1] + 3 * v[i]) * self.hs[i - 1]
-    #        else:
-    #            K[i, i] = (v[i - 1] + 3 * v[i]) * self.hs[i - 1] + (
-    #                3 * v[i] + v[i + 1]
-    #            ) * self.hs[i]
-    #            K[i, i - 1] = (v[i - 1] + v[i]) * self.hs[i - 1]
-    #            K[i, i + 1] = (v[i] + v[i + 1]) * self.hs[i]
-    #    return K / 12
-
-    def get_K1(self, v):
-        K = np.zeros((self.size, self.size))
+    def get_V(self):
+        V = np.zeros((self.size, self.size))
         for i in range(self.size):
             if i == 0:
-                K[i, i] = self.hs[i] / 3 * v[i]
-                K[i, i + 1] = self.hs[i] / 6 * v[i]
+                V[i, i] = (3 * self.v[i] + self.v[i + 1]) * self.hs[i]
+                V[i, i + 1] = (self.v[i] + self.v[i + 1]) * self.hs[i]
             elif i == self.size - 1:
-                K[i, i - 1] = self.hs[i - 1] / 6 * v[i - 1]
-                K[i, i] = self.hs[i - 1] / 3 * v[i - 1]
+                V[i, i - 1] = (self.v[i - 1] + self.v[i]) * self.hs[i - 1]
+                V[i, i] = (self.v[i - 1] + 3 * self.v[i]) * self.hs[i - 1]
             else:
-                K[i, i - 1] = self.hs[i] / 6 * v[i - 1]
-                K[i, i] = self.hs[i] / 3 * (v[i - 1] + v[i])
-                K[i, i + 1] = self.hs[i] / 6 * v[i]
-        return K
+                V[i, i] = (self.v[i - 1] + 3 * self.v[i]) * self.hs[i - 1] + (
+                    3 * self.v[i] + self.v[i + 1]
+                ) * self.hs[i]
+                V[i, i - 1] = (self.v[i - 1] + self.v[i]) * self.hs[i - 1]
+                V[i, i + 1] = (self.v[i] + self.v[i + 1]) * self.hs[i]
+        return V / 12
